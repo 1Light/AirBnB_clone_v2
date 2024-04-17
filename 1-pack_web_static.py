54fd92f8ddc3e6744be10e18cb9d16080adea379
1Light <redclover1.618@gmail.com>
2024-04-17 17:22:56 +0300

    upload

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
-"""A module for web application deployment with Fabric."""
+from fabric.api import *
 import os
 from datetime import datetime
-from fabric.api import local, runs_once
+
+env.hosts = ['localhost']
 
 
-@runs_once
 def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
     try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
+        filepath = "versions/web_static_" + datetime.now().\
+                   strftime("%Y%m%d%H%M%S") + ".tgz"
+        local("mkdir -p versions")
+        local("tar -zcvf versions/web_static_$(date +%Y%m%d%H%M%S).tgz\
+        web_static")
+        print("web_static packed: {} -> {}".
+              format(filepath, os.path.getsize(filepath)))
+    except:
+            return None
